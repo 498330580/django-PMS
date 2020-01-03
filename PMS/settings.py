39eab7141225cb18eb08eaf2bc13963fb726b878
学海无涯92124 <498330580@qq.com>
2020-01-03 17:56:45 +0800

    修改设置

--- PMS/settings.py
+++ PMS/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = 'asftav7_jwdo=^et@mm@nmhc+udy+7xv$q=%i5&15!aai+m4dy'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 
